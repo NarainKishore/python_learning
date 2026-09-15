8e2ad51fe9bc2cef84ab50b7e90377292b78df35
NarainKishore <narainkishore007@gmail.com>
2026-09-15 21:31:52 +0530

    Day 1: Basics - variables, data types, arithmetic, strings, input

---
+++ day1_basics.py
@@ -0,0 +1,43 @@
+#variables
+name = 'Kishore'
+age = 21
+city = 'chennai'
+
+print(f'My name is {name},I am {age}, I live in {city}')
+
+#datatypes
+
+x = 10
+y = 3.5
+z = "Hello" 
+flag = True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(flag))
+
+#Arithmetic 
+a = 5
+b = 2
+print(a+b , a-b, a*b , a/b)
+
+
+#String operations
+word_1 = 'hello'
+word_2 = 'Mario'
+
+print(word_1 + word_2)
+
+word = 'hello world python'
+print(word[0:3])
+
+print(word.upper())
+print(word.lower())
+
+
+name_1 = str(input("Enter your Name: "))
+age_1 = int(input("Enter your age: "))
+print(f'Hello {name_1}, you are {age_1} years old!')
+
+
