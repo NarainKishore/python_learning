da2c0c2929db669e74b3f1699617eedc4238d37b
NarainKishore <narainkishore007@gmail.com>
2026-09-16 21:26:47 +0530

    Day 2: Input/Output and Type Conversion

---
+++ day2_basics.py
@@ -0,0 +1,35 @@
+'''num_1 = int(input("Enter any number 1: "))
+num_2 = int(input("Enter any number 2: "))
+
+print(f"Answer is :{num_1 + num_2}")
+
+length = int(input('Enter length: '))
+width = int(input('Enter width: '))
+
+print(f"Area of a Rectangle:{length * width}")
+
+
+num = int(input("Enter a number: "))
+print(num * 2)
+
+
+p = float(input('Enter Principle: '))
+t = float(input("Enter a Time (years): "))
+r = float(input('Enter Rate (%):  '))
+
+si = (p * t * r) / 100
+
+print(f"Simple Interest = {si}")'''
+
+
+num = int('123')
+print(num * 10)
+
+num = float('12.5')
+print(num * 2)
+
+age = 21
+print('Iam ' + str(age) + ' years old')
+
+print(bool("Hello"))   
+print(bool(""))
